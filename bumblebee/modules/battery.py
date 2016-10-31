293c8453be469f0680f18877b0d968c7ec72a0e7
Tobias Witek <tobi@tobi-wan-kenobi.at>
2016-10-31 12:01:21 +0100

    [modules/battery] Add warning and critical functionality for battery

--- bumblebee/modules/battery.py
+++ bumblebee/modules/battery.py
@@ -14,6 +14,12 @@ class Module(bumblebee.module.Module):
 
         return "{:02d}%".format(self._capacity)
 
+    def warning(self):
+        return self._capacity < 20
+
+    def critical(self):
+        return self._capacity < 10
+
     def state(self):
         with open("/sys/class/power_supply/{}/status".format(self._battery)) as f:
             self._status = f.read().strip()
