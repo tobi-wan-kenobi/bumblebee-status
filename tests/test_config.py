df273559776a22d68d53743162e751221ce4c5f4
Tobias Witek <tobi@tobi-wan-kenobi.at>
2016-11-05 07:54:16 +0100

    [tests] Add framework for unittests

---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+import unittest
+
+import bumblebee.config
+
+class TestConfigCreation(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
