ba466d51797f44734b04d171aab253b58b46b9c7
Tobi-wan Kenobi <github@tobi-wan-kenobi.at>
2016-11-09 19:37:09 +0100

    [modules/brightness] Add callbacks for increasing/decreasing brightness
    Use xbacklight to increase or decrease brightness when the user uses the
    mouse wheel over the brightness widget.

--- bumblebee/modules/brightness.py
+++ bumblebee/modules/brightness.py
@@ -3,12 +3,22 @@ import bumblebee.module
 def description():
     return "Displays brightness percentage"
 
+def parameters():
+    return [
+        "brightness.step: Steps (in percent) to increase/decrease brightness on scroll (defaults to 2)",
+    ]
+
 class Module(bumblebee.module.Module):
     def __init__(self, output, config, alias):
         super(Module, self).__init__(output, config, alias)
         self._brightness = 0
         self._max = 0
         self._percent = 0
+
+        step = self._config.parameter("step", 2)
+
+        output.add_callback(module=self.instance(), button=4, cmd="xbacklight +{}%".format(step))
+        output.add_callback(module=self.instance(), button=5, cmd="xbacklight -{}%".format(step))
 
     def widgets(self):
         with open("/sys/class/backlight/intel_backlight/brightness") as f:
@@ -20,3 +30,4 @@ class Module(bumblebee.module.Module):
 
         return bumblebee.output.Widget(self, "{:02d}%".format(self._percent))
 
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
