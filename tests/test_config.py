e5201187a29a2189481b80db76395e1e613920f9
Tobi-wan Kenobi <github@tobi-wan-kenobi.at>
2016-12-04 08:25:11 +0100

    [tests/config] Small refactoring (renaming)

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,17 +9,16 @@ class TestConfig(unittest.TestCase):
         self.someSimpleModules = ["foo", "bar", "baz"]
         self.someAliasModules = ["foo:a", "bar:b", "baz:c"]
 
-
     def test_no_modules_by_default(self):
         self.assertEquals(self.defaultConfig.modules(), [])
 
-    def test_load_simple_modules(self):
+    def test_simple_modules(self):
         cfg = Config(["-m"] + self.someSimpleModules)
         self.assertEquals(cfg.modules(), [{
             "name": x, "module": x
         } for x in self.someSimpleModules])
 
-    def test_load_alias_modules(self):
+    def test_alias_modules(self):
         cfg = Config(["-m"] + self.someAliasModules)
         self.assertEquals(cfg.modules(), [{
             "module": x.split(":")[0],
