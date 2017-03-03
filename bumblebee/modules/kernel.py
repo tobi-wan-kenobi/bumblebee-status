955f123b888bf45b9c1afc8d97a50ec89974f6c7
Tobi-wan Kenobi <github@tobi-wan-kenobi.at>
2017-03-03 19:13:30 +0100

    [modules/kernel] Add documentation and vim hints

--- bumblebee/modules/kernel.py
+++ bumblebee/modules/kernel.py
@@ -1,3 +1,7 @@
+# pylint: disable=C0111,R0903
+
+"""Shows Linux kernel version information"""
+
 import bumblebee.input
 import bumblebee.output
 import bumblebee.engine
@@ -11,3 +15,5 @@ class Module(bumblebee.engine.Module):
 
     def output(self, widget):
         return self._release_name
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
