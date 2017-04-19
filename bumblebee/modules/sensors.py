2052f58dde1347cea549e08cd3d9e15137a604d1
Tobias Witek <tobi@tobi-wan-kenobi.at>
2017-04-19 18:53:45 +0200

    [modules/sensors] Minor cosmetic changes + screenshot
    Cosmetic changes:
    * replace all single ticks with double ticks
    * add vim line
    
    Added screenshot for sensors module.

--- bumblebee/modules/sensors.py
+++ bumblebee/modules/sensors.py
@@ -20,9 +20,9 @@ class Module(bumblebee.engine.Module):
         super(Module, self).__init__(engine, config,
             bumblebee.output.Widget(full_text=self.temperature)
         )
-        self._temperature = 'unknown'
+        self._temperature = "unknown"
         pattern = self.parameter("match", "temp1_input")
-        pattern_string = r'^\s*{}:\s*([\d.]+)$'.format(pattern)
+        pattern_string = r"^\s*{}:\s*([\d.]+)$".format(pattern)
         self._match_number = int(self.parameter("match_number", "-1"))
         self._pattern = re.compile(pattern_string, re.MULTILINE)
         engine.input.register_callback(self, button=bumblebee.input.LEFT_MOUSE, cmd="xsensors")
@@ -30,7 +30,7 @@ class Module(bumblebee.engine.Module):
     def get_temp(self):
         temperatures = bumblebee.util.execute("sensors -u")
         matching_temp = self._pattern.findall(temperatures)
-        temperature = 'unknown'
+        temperature = "unknown"
         if matching_temp:
             temperature = matching_temp[self._match_number]
 
@@ -41,3 +41,5 @@ class Module(bumblebee.engine.Module):
 
     def update(self, widgets):
         self._temperature = self.get_temp()
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
