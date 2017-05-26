c3789a13992fa16e63786ed482317842304b1ae2
Yves Hoppe <yves.hoppe@community.joomla.org>
2017-05-26 21:14:05 +0200

    [modules/spotify] Add rudimentary spotify currently playing module

---
+++ bumblebee/modules/spotify.py
@@ -0,0 +1,41 @@
+# pylint: disable=C0111,R0903
+
+"""Displays the current song being played
+
+Requires the following library:
+    * python-dbus
+
+Parameters:
+"""
+
+import bumblebee.input
+import bumblebee.output
+import bumblebee.engine
+
+try:
+    import dbus
+except ImportError:
+    pass
+
+
+class Module(bumblebee.engine.Module):
+    def __init__(self, engine, config):
+        super(Module, self).__init__(engine, config,
+                                     bumblebee.output.Widget(full_text=self.spotify)
+                                     )
+        self._song = ""
+
+    def spotify(self, widget):
+        return str(self._song)
+
+    def update(self, widgets):
+        try:
+            bus = dbus.SessionBus()
+            spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+            spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
+            props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+            self._song = (str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
+        except dbus.exceptions.DBusException:
+            self._song = "Error: DBus Exception"
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
