6d4968571592274c084675486919a49e1edbb89c
Bernhard B <schluchti@gmail.com>
2019-05-31 18:19:37 +0200

    improved error handling
    * log a warning message in case tkinter is not installed

--- bumblebee/popup.py
+++ bumblebee/popup.py
@@ -1,5 +1,7 @@
 """Pop-up menus."""
 
+import logging
+
 try:
     import Tkinter as tk
 except ImportError:
@@ -7,9 +9,7 @@ except ImportError:
     try:
         import tkinter as tk
     except ImportError:
-        pass
-
-import logging
+        logging.warning("failed to import tkinter - bumblebee popups won't work!")
 
 class PopupMenu:
     """The popup-menu."""
