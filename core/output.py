a17cd4759c71f2b15112d1120c1ae6473f59cbc3
Tobias Witek <tobi@tobi-wan-kenobi.at>
2020-01-26 13:31:20 +0100

    [core/output] Implement simple wait
    To pace the output lines, implement a simple wait() call for the i3
    output.

--- core/output.py
+++ core/output.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 class i3(object):
     def start(self):
@@ -12,5 +13,8 @@ class i3(object):
 
     def end_status_line(self):
         return '],\n'
+
+    def wait(self, interval):
+        time.sleep(interval)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
