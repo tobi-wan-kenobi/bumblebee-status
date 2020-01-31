e2bc26352080e49b36fa89b40b19beb6f77c8d9d
me <me@localhost>
2020-01-31 11:19:59 +0200

    [fix] always return a list, even empty

--- bumblebee/output.py
+++ bumblebee/output.py
@@ -344,10 +344,10 @@ class WidgetDrawer(object):
         """
         full_text = widget.full_text()
         if widget.get_module() and widget.get_module().hidden():
-            return
+            return []
         if widget.get_module() and widget.get_module().name in self._config.autohide():
             if not any(state in widget.state() for state in ["warning", "critical"]):
-                return
+                return []
         padding = self._theme.padding(widget)
 
         prefix = self._theme.prefix(widget, padding)
