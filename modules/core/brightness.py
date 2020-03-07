cfa4d0df48bfb81cf632b155210f6b282b82dd18
Tobias Witek <tobi@tobi-wan-kenobi.at>
2020-03-07 13:34:22 +0100

    [modules/brightness] Single quotes

--- modules/core/brightness.py
+++ modules/core/brightness.py
@@ -1,12 +1,12 @@
 # pylint: disable=C0111,R0903
 
-"""Displays the brightness of a display
+'''Displays the brightness of a display
 
 Parameters:
     * brightness.step: The amount of increase/decrease on scroll in % (defaults to 2)
     * brightness.device_path: The device path (defaults to /sys/class/backlight/intel_backlight), can contain wildcards (in this case, the first matching path will be used)
 
-"""
+'''
 
 import bumblebee.util
 import bumblebee.input
@@ -21,18 +21,18 @@ class Module(bumblebee.engine.Module):
                                      bumblebee.output.Widget(full_text=self.brightness))
         self._brightness = 0
 
-        self._device_path = self.find_device(self.parameter("device_path", "/sys/class/backlight/intel_backlight"))
-        step = self.parameter("step", 2)
+        self._device_path = self.find_device(self.parameter('device_path', '/sys/class/backlight/intel_backlight'))
+        step = self.parameter('step', 2)
 
-        if bumblebee.util.which("light"):
-            self.register_cmd(engine, "light -A {}%".format(step),
-                              "light -U {}%".format(step))
-        elif bumblebee.util.which("brightnessctl"):
-            self.register_cmd(engine, "brightnessctl s {}%+".format(step),
-                              "brightnessctl s {}%-".format(step))
+        if bumblebee.util.which('light'):
+            self.register_cmd(engine, 'light -A {}%'.format(step),
+                              'light -U {}%'.format(step))
+        elif bumblebee.util.which('brightnessctl'):
+            self.register_cmd(engine, 'brightnessctl s {}%+'.format(step),
+                              'brightnessctl s {}%-'.format(step))
         else:
-            self.register_cmd(engine, "xbacklight +{}%".format(step),
-                              "xbacklight -{}%".format(step))
+            self.register_cmd(engine, 'xbacklight +{}%'.format(step),
+                              'xbacklight -{}%'.format(step))
 
     def find_device(self, device_path):
         res = glob.glob(device_path)
@@ -46,18 +46,18 @@ class Module(bumblebee.engine.Module):
 
     def brightness(self, widget):
         if isinstance(self._brightness, float):
-            return "{:3.0f}%".format(self._brightness).strip()
+            return '{:3.0f}%'.format(self._brightness).strip()
         else:
-            return "n/a"
+            return 'n/a'
 
     def update(self, widgets):
         try:
-            with open("{}/brightness".format(self._device_path)) as f:
+            with open('{}/brightness'.format(self._device_path)) as f:
                 backlight = int(f.readline())
-            with open("{}/max_brightness".format(self._device_path)) as f:
+            with open('{}/max_brightness'.format(self._device_path)) as f:
                 max_brightness = int(f.readline())
                 self._brightness = float(backlight * 100 / max_brightness)
         except:
-            return "Error"
+            return 'Error'
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
