4bc5f73ccc3687033fd61f2f81d12c7423ef97b4
Tobias Witek <tobi@tobi-wan-kenobi.at>
2020-03-07 13:49:09 +0100

    [modules/dunst] Single quotes

--- modules/contrib/dunst.py
+++ modules/contrib/dunst.py
@@ -1,6 +1,6 @@
 #pylint: disable=C0111,R0903
 
-"""Toggle dunst notifications."""
+'''Toggle dunst notifications.'''
 
 import bumblebee.input
 import bumblebee.output
@@ -10,12 +10,12 @@ import bumblebee.engine
 class Module(bumblebee.engine.Module):
     def __init__(self, engine, config):
         super(Module, self).__init__(engine, config,
-                bumblebee.output.Widget(full_text="")
+                bumblebee.output.Widget(full_text='')
         )
         self._paused = False
         # Make sure that dunst is currently not paused
         try:
-            bumblebee.util.execute("killall -SIGUSR2 dunst")
+            bumblebee.util.execute('killall -SIGUSR2 dunst')
         except:
             pass
         engine.input.register_callback(self, button=bumblebee.input.LEFT_MOUSE,
@@ -27,13 +27,13 @@ class Module(bumblebee.engine.Module):
         
         try:
             if self._paused:
-                bumblebee.util.execute("killall -SIGUSR1 dunst")
+                bumblebee.util.execute('killall -SIGUSR1 dunst')
             else:
-                bumblebee.util.execute("killall -SIGUSR2 dunst")
+                bumblebee.util.execute('killall -SIGUSR2 dunst')
         except:
             self._paused = not self._paused # toggling failed
 
     def state(self, widget):
         if self._paused:
-            return ["muted", "warning"]
-        return ["unmuted"]
+            return ['muted', 'warning']
+        return ['unmuted']
