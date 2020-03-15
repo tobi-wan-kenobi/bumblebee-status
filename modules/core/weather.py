0e538a60883cc01ed8aafd2b8d0c94a2b52b58de
Tobias Witek <tobi@tobi-wan-kenobi.at>
2020-03-15 14:05:26 +0100

    [modules/weather] double quotes to single quotes

--- modules/core/weather.py
+++ modules/core/weather.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # pylint: disable=C0111,R0903
 
-"""Displays the temperature on the current location based on the ip
+'''Displays the temperature on the current location based on the ip
 
 Requires the following python packages:
     * requests
@@ -14,7 +14,7 @@ Parameters:
     * weather.showcity: If set to true, show location information, otherwise hide it (defaults to true)
     * weather.showminmax: If set to true, show the minimum and maximum temperature, otherwise hide it (defaults to false)
     * weather.apikey: API key from http://api.openweathermap.org
-"""
+'''
 
 import bumblebee.input
 import bumblebee.output
@@ -33,16 +33,16 @@ class Module(bumblebee.engine.Module):
             bumblebee.output.Widget(full_text=self.output)
         )
         self._temperature = 0
-        self._apikey = self.parameter("apikey", "af7bfe22287c652d032a3064ffa44088")
-        self._location = self.parameter("location", "auto")
-        if "," in self._location:
-            self._location = self._location.split(",")
+        self._apikey = self.parameter('apikey', 'af7bfe22287c652d032a3064ffa44088')
+        self._location = self.parameter('location', 'auto')
+        if ',' in self._location:
+            self._location = self._location.split(',')
         else:
             self._location = [self._location]
         self._index = 0
-        self._showcity = bumblebee.util.asbool(self.parameter("showcity", True))
-        self._showminmax = bumblebee.util.asbool(self.parameter("showminmax", False))
-        self._unit = self.parameter("unit", "metric")
+        self._showcity = bumblebee.util.asbool(self.parameter('showcity', True))
+        self._showminmax = bumblebee.util.asbool(self.parameter('showminmax', False))
+        self._unit = self.parameter('unit', 'metric')
         self._valid = False
         self.interval_factor(60)
         self.interval(15)
@@ -59,33 +59,33 @@ class Module(bumblebee.engine.Module):
         self.update(self.widgets())
 
     def _unit_suffix(self):
-        if self._unit == "metric":
-            return "C"
-        if self._unit == "kelvin":
-            return "K"
-        if self._unit == "imperial":
-            return "F"
-        return ""
+        if self._unit == 'metric':
+            return 'C'
+        if self._unit == 'kelvin':
+            return 'K'
+        if self._unit == 'imperial':
+            return 'F'
+        return ''
 
     def temperature(self):
-        return u"{}°{}".format(self._temperature, self._unit_suffix())
+        return u'{}°{}'.format(self._temperature, self._unit_suffix())
 
     def tempmin(self):
-        return u"{}°{}".format(self._tempmin, self._unit_suffix())
+        return u'{}°{}'.format(self._tempmin, self._unit_suffix())
 
     def tempmax(self):
-        return u"{}°{}".format(self._tempmax, self._unit_suffix())
+        return u'{}°{}'.format(self._tempmax, self._unit_suffix())
 
     def city(self):
         city = re.sub('[_-]', ' ', self._city)
-        return u"{} ".format(city)
+        return u'{} '.format(city)
 
     def output(self, widget):
         if not self._valid:
-            return u"?"
+            return u'?'
         if self._showminmax:
             self._showcity=False
-            return self.city() + self.temperature() + "  Hi:" + self.tempmax() + "  Lo:" + self.tempmin()
+            return self.city() + self.temperature() + '  Hi:' + self.tempmax() + '  Lo:' + self.tempmin()
         elif self._showcity:
             return self.city() + self.temperature()
         else:
@@ -93,19 +93,19 @@ class Module(bumblebee.engine.Module):
 
     def state(self, widget):
         if self._valid:
-            if "thunderstorm" in self._weather:
+            if 'thunderstorm' in self._weather:
                 return ['thunder']
-            elif "drizzle" in self._weather:
+            elif 'drizzle' in self._weather:
                 return ['rain']
-            elif "rain" in self._weather:
+            elif 'rain' in self._weather:
                 return ['rain']
-            elif "snow" in self._weather:
+            elif 'snow' in self._weather:
                 return ['snow']
-            elif "sleet" in self._weather:
+            elif 'sleet' in self._weather:
                 return ['sleet']
-            elif "clear" in self._weather:
+            elif 'clear' in self._weather:
                 return ['clear']
-            elif "cloud" in self._weather:
+            elif 'cloud' in self._weather:
                 return ['clouds']
             else:
                 return []
@@ -114,17 +114,17 @@ class Module(bumblebee.engine.Module):
 
     def update(self, widgets):
         try:
-            weather_url = "http://api.openweathermap.org/data/2.5/weather?appid={}".format(self._apikey)
-            weather_url = "{}&units={}".format(weather_url, self._unit)
-            if self._location[self._index] == "auto":
-                location_url = "http://ipinfo.io/json"
+            weather_url = 'http://api.openweathermap.org/data/2.5/weather?appid={}'.format(self._apikey)
+            weather_url = '{}&units={}'.format(weather_url, self._unit)
+            if self._location[self._index] == 'auto':
+                location_url = 'http://ipinfo.io/json'
                 location = requests.get(location_url).json()
-                coord = location["loc"].split(",")
-                weather_url = "{url}&lat={lat}&lon={lon}".format(url=weather_url, lat=coord[0], lon=coord[1])
+                coord = location['loc'].split(',')
+                weather_url = '{url}&lat={lat}&lon={lon}'.format(url=weather_url, lat=coord[0], lon=coord[1])
             elif self._location[self._index].isdigit():
-                weather_url = "{url}&id={id}".format(url=weather_url, id=self._location[self._index])
+                weather_url = '{url}&id={id}'.format(url=weather_url, id=self._location[self._index])
             else:
-                weather_url = "{url}&q={city}".format(url=weather_url, city=self._location[self._index])
+                weather_url = '{url}&q={city}'.format(url=weather_url, city=self._location[self._index])
             weather = requests.get(weather_url).json()
             self._city = weather['name']
             self._temperature = int(weather['main']['temp'])
