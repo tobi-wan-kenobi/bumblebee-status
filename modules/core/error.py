2b8eda9d7247add5dfebe360a80fcb0baddc521f
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2020-04-07 20:44:10 +0200

    [modules/error] Add simple module to show bumblebee errors

---
+++ modules/core/error.py
@@ -0,0 +1,30 @@
+# pylint: disable=C0111,R0903
+
+"""Shows bumblebee-status errors"""
+
+import platform
+
+import core.module
+import core.widget
+import core.event
+
+class Module(core.module.Module):
+    def __init__(self, config):
+        super().__init__(config, core.widget.Widget(self.full_text))
+        self.__error = ''
+        self.__state = 'critical'
+
+        core.event.register('error', self.__set_error)
+
+    def full_text(self, widgets):
+        return self.__error
+
+    def __set_error(self, error='n/a', state='critical'):
+        self.__error = error
+        self.__state = state
+        self.update()
+
+    def state(self, widget):
+        if self.__error: return [self.__state]
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
