05a3c5d8f6de59f25a8ceba9a47c11bbba130444
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2020-04-13 13:57:24 +0200

    [modules/todo] Update to latest API

--- modules/contrib/todo.py
+++ modules/contrib/todo.py
@@ -6,40 +6,38 @@ Parameters:
     * todo.file: File to read TODOs from (defaults to ~/Documents/todo.txt)
 """
 
-import bumblebee.input
-import bumblebee.output
-import bumblebee.engine
 import os.path
 
+import core.module
+import core.widget
 
-class Module(bumblebee.engine.Module):
+class Module(core.module.Module):
+    def __init__(self, config):
+        super().__init__(config, core.widget.Widget(self.output))
 
-
-    def __init__(self, engine, config):
-        super(Module, self).__init__(engine, config,
-            bumblebee.output.Widget(full_text=self.output)
-        )
-        self._doc = os.path.expanduser(self.parameter("file", "~/Documents/todo.txt"))
-        self._todos = self.count_items()
+        self.__doc = os.path.expanduser(self.parameter('file', '~/Documents/todo.txt'))
+        self.__todos = self.count_items()
 
 
     def output(self, widget):
-       self._todos = self.count_items()
-       return str(self._todos)
+       return str(self.__todos)
 
+    def update(self):
+       self.__todos = self.count_items()
 
     def state(self, widgets):
-        if self._todos == 0:
-            return "empty"
-        return "items"
-
+        if self.__todos == 0:
+            return 'empty'
+        return 'items'
 
     def count_items(self):
         try:
             i = -1
-            with open(self._doc) as f:
+            with open(self.__doc) as f:
                 for i, l in enumerate(f):
                     pass
             return i+1
         except Exception:
             return 0
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
