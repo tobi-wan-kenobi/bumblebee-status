e7b9a0e2f81f1fedeb45f5569e508ff5f5425791
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2020-04-13 09:32:29 +0200

    [modules/uptime] Update to new API

--- modules/contrib/uptime.py
+++ modules/contrib/uptime.py
@@ -2,29 +2,22 @@
 
 """Displays the system uptime."""
 
-# Use absolute_import because there's already a datatime module
-# in the same directory
-from __future__ import absolute_import
-
-import bumblebee.input
-import bumblebee.output
-import bumblebee.engine
-
 from datetime import timedelta
 
-class Module(bumblebee.engine.Module):
-    def __init__(self, engine, config):
-        super(Module, self).__init__(engine, config,
-            bumblebee.output.Widget(full_text=self.output)
-        )
-        self._uptime = ""
+import core.module
+import core.widget
+
+class Module(core.module.Module):
+    def __init__(self, config):
+        super().__init__(config, core.widget.Widget(self.output))
+        self.__uptime = ''
 
     def output(self, _):
-        return "{}".format(self._uptime)
+        return '{}'.format(self.__uptime)
 
-    def update(self, widgets):
+    def update(self):
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = int(float(f.readline().split()[0]))
-            self._uptime = timedelta(seconds = uptime_seconds)
+            self.__uptime = timedelta(seconds = uptime_seconds)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
