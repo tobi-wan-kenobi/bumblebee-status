10a1bc18ad3cbc45711ab5731469a3365bbe9beb
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2020-04-19 10:14:56 +0200

    [modules/gpmdp] quotes

--- modules/contrib/gpmdp.py
+++ modules/contrib/gpmdp.py
@@ -14,43 +14,43 @@ import bumblebee.engine
 class Module(bumblebee.engine.Module):
     def __init__(self, engine, config):
         widgets = [
-            bumblebee.output.Widget(name="gpmdp.prev"),
-            bumblebee.output.Widget(name="gpmdp.main", full_text=self.description),
-            bumblebee.output.Widget(name="gpmdp.next"),
+            bumblebee.output.Widget(name='gpmdp.prev'),
+            bumblebee.output.Widget(name='gpmdp.main', full_text=self.description),
+            bumblebee.output.Widget(name='gpmdp.next'),
         ]
         super(Module, self).__init__(engine, config, widgets)
 
         engine.input.register_callback(widgets[0], button=bumblebee.input.LEFT_MOUSE,
-            cmd="playerctl previous")
+            cmd='playerctl previous')
         engine.input.register_callback(widgets[1], button=bumblebee.input.LEFT_MOUSE,
-             cmd="playerctl play-pause")
+             cmd='playerctl play-pause')
         engine.input.register_callback(widgets[2], button=bumblebee.input.LEFT_MOUSE,
-             cmd="playerctl next")
+             cmd='playerctl next')
 
         self._status = None
         self._tags = None
 
     def description(self, widget):
-        return self._tags if self._tags else "n/a"
+        return self._tags if self._tags else 'n/a'
 
     def update(self, widgets):
         self._load_song()
 
     def state(self, widget):
-        if widget.name == "gpmdp.prev":
-            return "prev"
-        if widget.name == "gpmdp.next":
-            return "next"
+        if widget.name == 'gpmdp.prev':
+            return 'prev'
+        if widget.name == 'gpmdp.next':
+            return 'next'
         return self._status
 
     def _load_song(self):
-        info = ""
+        info = ''
         try:
-            info = bumblebee.util.execute("gpmdp-remote current")
-            status = bumblebee.util.execute("gpmdp-remote status")
+            info = bumblebee.util.execute('gpmdp-remote current')
+            status = bumblebee.util.execute('gpmdp-remote status')
         except RuntimeError:
             pass
-        self._status = status.split("\n")[0].lower()
-        self._tags = info.split("\n")[0]
+        self._status = status.split('\n')[0].lower()
+        self._tags = info.split('\n')[0]
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
