2028d51e15da5dc8b167be8e6a6cea31992170b0
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2020-04-29 20:20:04 +0200

    [modules/bluetooth2] quotes

--- modules/contrib/bluetooth2.py
+++ modules/contrib/bluetooth2.py
@@ -29,8 +29,8 @@ class Module(bumblebee.engine.Module):
                                      bumblebee.output.Widget(
                                          full_text=self.status))
 
-        self.manager = self.parameter("manager", "blueman-manager")
-        self._status = "Off"
+        self.manager = self.parameter('manager', 'blueman-manager')
+        self._status = 'Off'
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         self._bus = dbus.SystemBus()
 
@@ -48,12 +48,12 @@ class Module(bumblebee.engine.Module):
         state = len(subprocess.run(['bluetoothctl', 'list'], stdout=subprocess.PIPE).stdout)
         if state > 0:
             connected_devices = self.get_connected_devices()
-            self._status = "On - {}".format(connected_devices)
+            self._status = 'On - {}'.format(connected_devices)
         else:
-            self._status = "Off"
+            self._status = 'Off'
             adapters_cmd = 'rfkill list | grep Bluetooth'
             if not len(subprocess.run(adapters_cmd, shell=True, stdout=subprocess.PIPE).stdout):
-                self._status = "No Adapter Found"
+                self._status = 'No Adapter Found'
         return
 
     def manager(self, widget):
@@ -62,12 +62,12 @@ class Module(bumblebee.engine.Module):
 
     def _toggle(self, widget=None):
         """Toggle bluetooth state."""
-        if "On" in self._status:
-            state = "false"
+        if 'On' in self._status:
+            state = 'false'
         else:
-            state = "true"
+            state = 'true'
 
-        cmd = "dbus-send --system --print-reply --dest=org.blueman.Mechanism /org/blueman/mechanism org.blueman.Mechanism.SetRfkillState boolean:%s" % state 
+        cmd = 'dbus-send --system --print-reply --dest=org.blueman.Mechanism /org/blueman/mechanism org.blueman.Mechanism.SetRfkillState boolean:%s' % state 
 
         logging.debug('bt: toggling bluetooth')
         bumblebee.util.execute(cmd)
@@ -76,29 +76,29 @@ class Module(bumblebee.engine.Module):
         """Get current state."""
         state = []
 
-        if self._status == "No Adapter Found":
-            state.append("critical")
-        elif self._status == "On - 0":
-            state.append("warning")
-        elif "On" in self._status and not(self._status == "On - 0"):
-            state.append("ON")
+        if self._status == 'No Adapter Found':
+            state.append('critical')
+        elif self._status == 'On - 0':
+            state.append('warning')
+        elif 'On' in self._status and not(self._status == 'On - 0'):
+            state.append('ON')
         else:
-            state.append("critical")
+            state.append('critical')
         return state
 
     def get_connected_devices(self):
         devices = 0
         objects = dbus.Interface(
-            self._bus.get_object("org.bluez", "/"),
-            "org.freedesktop.DBus.ObjectManager"
+            self._bus.get_object('org.bluez', '/'),
+            'org.freedesktop.DBus.ObjectManager'
         ).GetManagedObjects()
         for path, interfaces in objects.items():
-            if "org.bluez.Device1" in interfaces:
+            if 'org.bluez.Device1' in interfaces:
                 if dbus.Interface(
-                    self._bus.get_object("org.bluez", path),
-                    "org.freedesktop.DBus.Properties"
+                    self._bus.get_object('org.bluez', path),
+                    'org.freedesktop.DBus.Properties'
                 ).Get(
-                    "org.bluez.Device1", "Connected"
+                    'org.bluez.Device1', 'Connected'
                 ):
                     devices += 1
         return devices
