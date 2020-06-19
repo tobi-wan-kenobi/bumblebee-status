52f5315ec5cc877a56585ea969fa06398e616775
Ambika Nair <cepheac@protonmail.ch>
2020-06-19 06:12:30 -0400

    Added more tests for mpd module

--- pytests/modules/contrib/test_mpd.py
+++ pytests/modules/contrib/test_mpd.py
@@ -7,10 +7,26 @@ import modules.contrib.mpd
 def mpd_module():
     return modules.contrib.mpd.Module(config=core.config.Config([]), theme=None)
 
-def test_shuffle_off_by_default(mpd_module):
-    assert not mpd_module._shuffle
-
-def test_shuffle_state(mocker, mpd_module):
+def _test_state(mocker, mpd_module, widget_name, expected_output):
     widget = mocker.Mock()
-    widget.name = 'mpd.shuffle'
-    assert mpd_module.state(widget) == 'shuffle-off'
+    widget.name = widget_name
+    return mpd_module.state(widget) == expected_output
+
+def test_states(mocker, mpd_module):
+    assert _test_state(mocker, mpd_module, 'mpd.repeat', 'repeat-off')
+    assert _test_state(mocker, mpd_module, 'mpd.shuffle', 'shuffle-off')
+    assert _test_state(mocker, mpd_module, 'mpd.prev', 'prev')
+    assert _test_state(mocker, mpd_module, 'mpd.next', 'next')
+
+def test_no_host(mpd_module):
+    assert mpd_module._hostcmd == ''
+
+def test_host():
+    module_with_host = modules.contrib.mpd.Module(config=core.config.Config(['-p', 'mpd.host=sample-host']), theme=None)
+    assert module_with_host._hostcmd == ' -h sample-host'
+
+def test_bad_layout():
+    pytest.raises(KeyError, modules.contrib.mpd.Module, config=core.config.Config(['-p', 'mpd.layout="mpd.inexistent"']), theme=None)
+
+def test_hidden_on_creation(mpd_module):
+    assert mpd_module.hidden()
