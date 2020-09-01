213c28992cebe03b3b0143b831c005d620503510
Thaynã Moretti <moretty@gmail.com>
2020-09-01 20:34:18 -0300

    Add date tests

--- tests/modules/core/test_date.py
+++ tests/modules/core/test_date.py
@@ -1,5 +1,56 @@
 import pytest
+from unittest import TestCase, mock
+from freezegun import freeze_time
 
-def test_load_module():
-    __import__("modules.core.date")
+import core.config
+import core.input
+import core.widget
+import modules.core.date
+
+def build_module(args = []):
+    config = core.config.Config(args)
+    return modules.core.date.Module(config=config, theme=None)
+
+def build_widget():
+    return core.widget.Widget()
+
+class DateTest(TestCase):
+    def setup_class(self):
+        locale_patcher = mock.patch('locale.getdefaultlocale')
+        locale_mock = locale_patcher.start()
+        locale_mock.return_value = ('en-US', 'UTF-8')
+
+        self.widget = build_widget()
+
+    def test_load_module(self):
+        __import__("modules.core.date")
+
+    @freeze_time('2020-10-15 03:25:59')
+    def test_default_format(self):
+        module = build_module()
+        assert module.full_text(self.widget) == '10/15/2020'
+
+    @freeze_time('2020-10-20 12:30:00')
+    def test_custom_format(self):
+        module = build_module(['-p', 'date.format=%d.%m.%y'])
+        assert module.full_text(self.widget) == '20.10.20'
+
+    @freeze_time('2020-01-10 10:20:30')
+    @mock.patch('locale.getdefaultlocale')
+    def test_invalid_locale(self, locale_mock):
+        locale_mock.return_value = ('in-IN', 'UTF-0')
+
+        module = build_module()
+        assert module.full_text(self.widget) == '01/10/2020'
+
+    @mock.patch('core.input.register')
+    def test_register_left_mouse_action(self, input_register_mock):
+        module = build_module()
+
+        input_register_mock.assert_called_with(
+            module,
+            button=core.input.LEFT_MOUSE,
+            cmd='calendar'
+        )
+
 
