4df495601a8c8605b29a06eebf58cf95a9cca679
Thaynã Moretti <moretty@gmail.com>
2020-10-01 19:10:46 -0300

    Create symbolic links

---
+++ bumblebee_status/modules/contrib/layout_xkbswitch.py
@@ -0,0 +1,1 @@
+layout-xkbswitch.py
--- tests/modules/contrib/test_battery-upower.py
+++ tests/modules/contrib/test_battery-upower.py
@@ -5,3 +5,6 @@ pytest.importorskip("dbus")
 def test_load_module():
     __import__("modules.contrib.battery-upower")
 
+def test_load_symbolic_link_module():
+    __import__("modules.contrib.battery_upower")
+
