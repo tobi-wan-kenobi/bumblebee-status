74ecbb6ca8e3202cafc69bfbf332081a5eec85cb
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2021-11-05 19:11:45 +0100

    [core/output] fix logic error when using "autohide"
    - when state is critical or warning -> *show* the module
    - when state is mayhide -> *hide* the module
    
    see #835

--- bumblebee_status/core/output.py
+++ bumblebee_status/core/output.py
@@ -229,8 +229,10 @@ class i3(object):
         for widget in module.widgets():
             if widget.module and self.__config.autohide(widget.module.name):
                 if not any(
-                    state in widget.state() for state in ["warning", "critical", "mayhide"]
+                    state in widget.state() for state in ["warning", "critical"]
                 ):
+                    continue
+                if "mayhide" in widget.state():
                     continue
             if module.hidden():
                 continue
