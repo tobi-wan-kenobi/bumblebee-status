97a022e452d6183b6e0a3cdd1b3ca09b090b7fb1
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-09-14 21:12:01 +0200

    [modules/pulsectl] add friendly name support
    thanks to @anopheles for figuring out this is still missing

--- bumblebee_status/modules/core/pulsectl.py
+++ bumblebee_status/modules/core/pulsectl.py
@@ -96,7 +96,7 @@ class Module(core.module.Module):
             res = f"{res} {util.graph.hbar(self.__volume*100)}"
 
         if self.__show_device_name:
-            friendly_name = self.__devicename
+            friendly_name = self.parameter(self.__devicename, self.__devicename)
             icon = self.parameter("icon." + self.__devicename, "")
             res = (
                 icon + " " + friendly_name + " | " + res
