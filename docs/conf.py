20d67fd1bcd7766d142d2b811679e3ece8e750c2
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2023-12-27 12:41:40 +0100

    doc: add dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 html_logo = "logo.png"
 html_favicon = "favicon.ico"
 
