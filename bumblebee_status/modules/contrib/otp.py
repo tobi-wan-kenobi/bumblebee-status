09e7d42fcd3b3dd7cdaa5a36067f226834e58e7c
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-06 19:55:31 -0500

    feat: otp module - lint

--- bumblebee_status/modules/contrib/otp.py
+++ bumblebee_status/modules/contrib/otp.py
@@ -25,14 +25,13 @@ class Module(core.module.Module):
     def __init__(self, config, theme):
         super().__init__(config, theme, core.widget.Widget(self.full_text))
         self.__username = self.parameter("user_name", "not set")
-        self.__service_name = self.parameter("service_name", "not set") 
+        self.__service_name = self.parameter("service_name", "not set")
         self.__key = keyring.get_password(self.__service_name, self.__username)
         self.__totp = pyotp.TOTP(self.__key)
         core.input.register(
-            self, 
-            button=core.input.LEFT_MOUSE, cmd=self.copy_to_clipboard
+            self, button=core.input.LEFT_MOUSE, cmd=self.copy_to_clipboard
         )
-        
+
     def full_text(self, widget):
         if self.__service_name == "not set":
             return "ConfigError: otp.service_name not set"
@@ -42,6 +41,7 @@ class Module(core.module.Module):
 
     def copy_to_clipboard(self, event):
         import subprocess
+
         mfa_code = str(self.__totp.now())
         subprocess.Popen(["xclip", "-i"], stdin=subprocess.PIPE).communicate(
             mfa_code.encode("utf-8")
