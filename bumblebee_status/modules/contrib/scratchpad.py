2b1feee3c26727d54fefa33ce39c9eec4abb8a98
Tim Mackenzie <theymightbetim@gmail.com>
2025-03-27 13:14:41 -0400

    scratchpad module

---
+++ bumblebee_status/modules/contrib/scratchpad.py
@@ -0,0 +1,66 @@
+# pylint: disable=C0111,R0903
+
+"""Displays focused i3 window title.
+
+Requirements:
+    * i3ipc
+
+Parameters:
+    * 
+
+
+contributed by `UltimatePancake <https://github.com/UltimatePancake>`_ - many thanks!
+"""
+
+import threading
+
+try:
+    import i3ipc
+except ImportError:
+    pass
+
+import core.module
+import core.widget
+import core.input
+
+from util.rofi import showScratchpads
+
+
+class Module(core.module.Module):
+    def __init__(self, config, theme):
+        super().__init__(config, theme, core.widget.Widget(self.__getTitle))
+        core.input.register(self, button=core.input.LEFT_MOUSE, cmd=showScratchpads)
+
+        self.__scratchpads = 0
+        self.__title = f"{self.__scratchpads}"
+        # create a connection with i3ipc
+        self.__i3 = i3ipc.Connection()
+        self.__pollScratchpads()
+        # event is called both on fFocus change and title change, and on workspace change
+        for event in ["window::move", "window::urgent"]:
+            self.__i3.on(event, self.__pollScratchpads)
+        # begin listening for events
+        threading.Thread(target=self.__i3.main).start()
+
+
+    def __getTitle(self, widget):
+        return self.__title
+
+
+    def __pollScratchpads(self, *args, **kwargs):
+        root  = self.__i3.get_tree()
+        scratchpad = root.scratchpad()
+        if not scratchpad:
+            self.__scratchpads = 0
+            self.__title = f"no scratchpads found"
+            return
+
+        leaves = getattr(scratchpad, "floating_nodes", [])
+
+        count = len(leaves)
+
+        if count != self.__scratchpads:
+            self.__scratchpads = count
+            self.__title = f"{self.__scratchpads}"
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
---
+++ tests/util/test_rofi.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import MagicMock
+from bumblebee_status.util.rofi import showScratchpads
+
+class TestShowScratchpads(unittest.TestCase):
+    def test_showScratchpads(self):
+        global showScratchpads
+        temp = showScratchpads
+        showScratchpads = MagicMock()
+        showScratchpads(self=None)
+        showScratchpads.assert_called_once
+        showScratchpads = temp
